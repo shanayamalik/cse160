bb5c0cd949cfd0156291b463aca655d74d857cea
shanayamalik <shanaya.malik@gmail.com>
2024-04-04 18:14:58 -0700

    instantiated vector1 using vector3 class and called drawVector in main function

--- asg0/asg0.js
+++ asg0/asg0.js
@@ -10,7 +10,16 @@ function main() {
   // Get the rendering context for 2DCG
   var ctx = canvas.getContext('2d');
 
+  // Instantiate vector v1 with z coordinate set to 0
+    var v1 = new Vector3([2.25, 2.25, 0]);
+
+    // Call drawVector(v1, "red")
+    drawVector(v1, 'red', ctx);
+
+}
+
+/*
   // Draw a blue rectangle
   ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set a blue color
   ctx.fillRect(120, 10, 150, 150); // Fill a rectangle with the color
-}
+*/
