0b1e7678af8204f5f8b1b64ce959b56b926a9d9f
shanayamalik <shanaya.malik@gmail.com>
2024-04-06 23:41:09 -0700

    pushing local files to match remote

--- asg1/ColoredPoints.js
+++ asg1/ColoredPoints.js
@@ -71,6 +71,7 @@ function main() {
 
 var g_points = [];  // The array for the position of a mouse press
 var g_colors = [];  // The array to store the color of a point
+
 function click(ev) {
   var x = ev.clientX; // x coordinate of a mouse pointer
   var y = ev.clientY; // y coordinate of a mouse pointer
@@ -81,6 +82,7 @@ function click(ev) {
 
   // Store the coordinates to g_points array
   g_points.push([x, y]);
+
   // Store the coordinates to g_points array
   if (x >= 0.0 && y >= 0.0) {      // First quadrant
     g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
