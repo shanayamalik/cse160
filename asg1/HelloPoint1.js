e6c4c74af6ab22ae4d0f495de468a8c1b542bfbd
shanayamalik <shanaya.malik@gmail.com>
2024-04-06 22:24:26 -0700

    removed newlines and set vertex coordinates

--- asg1/HelloPoint1.js
+++ asg1/HelloPoint1.js
@@ -2,15 +2,15 @@
 // Vertex shader program
 var VSHADER_SOURCE = 
   'void main() {\n' +
-  '  gl_Position = vec4(0.0, 0.0, 0.0, 1.0);\n' + // Set the vertex coordinates of the point
+  '  gl_Position = vec4(0.5, 0.5, 0.0, 1.0);\n' + // Set the vertex coordinates of the point
   '  gl_PointSize = 10.0;\n' +                    // Set the point size
   '}\n';
 
 // Fragment shader program
-var FSHADER_SOURCE =
-  'void main() {\n' +
-  '  gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);\n' + // Set the point color
-  '}\n';
+var FSHADER_SOURCE = '
+  void main() { +
+    gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);\n' + // Set the point color
+  }';
 
 function main() {
   // Retrieve <canvas> element
