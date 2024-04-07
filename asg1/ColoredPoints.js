c698e331bd7efb8ceb606289d02086e59ee1c2ed
shanayamalik <shanaya.malik@gmail.com>
2024-04-07 12:03:38 -0700

    pushing all local files to update remote

--- asg1/ColoredPoints.js
+++ asg1/ColoredPoints.js
@@ -62,9 +62,9 @@ function addActionsForHtmlUI() {
   document.getElementById('green').onclick = function() {g_selectedColor = [0.0,1.0,0.0,1.0]; };
   document.getElementById('red').onclick = function() {g_selectedColor = [1.0,0.0,0.0,1.0]; };
 
-  document.getElementbyId('redSlide').addEventListener('mouseup', function() {g_selectedColor[0] = this.value/100; });
-  document.getElementbyId('greenSlide').addEventListener('mouseup', function() {g_selectedColor[1] = this.value/100; });  
-  document.getElementbyId('blueSlide').addEventListener('mouseup', function() {g_selectedColor[2] = this.value/100; });
+  document.getElementById('redSlide').addEventListener('mouseup', function() {g_selectedColor[0] = this.value/100; });
+  document.getElementById('greenSlide').addEventListener('mouseup', function() {g_selectedColor[1] = this.value/100; });  
+  document.getElementById('blueSlide').addEventListener('mouseup', function() {g_selectedColor[2] = this.value/100; });
 }
 
 function main() {
