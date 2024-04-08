f3b55a32cc42de1284b21e643321c19325225a3f
Shanaya Malik <139424427+shanayamalik@users.noreply.github.com>
2024-04-08 11:46:11 -0700

    Update ColoredPoints.js

--- asg1/ColoredPoints.js
+++ asg1/ColoredPoints.js
@@ -90,7 +90,9 @@ function main() {
 
   // Register function (event handler) to be called on a mouse press
   canvas.onmousedown = click;
-
+  canvas.onmousemove = click;
+  //canvas.onmousemove = function(ev) { if(ev.buttons == 1) { click(ev) } };
+  
   // Specify the color for clearing <canvas>
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
