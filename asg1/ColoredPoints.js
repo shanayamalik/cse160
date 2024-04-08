dfe4f6959fb0d2df7d81f01b37d1df833d4c5d92
Shanaya Malik <139424427+shanayamalik@users.noreply.github.com>
2024-04-08 11:07:34 -0700

    Update ColoredPoints.js

--- asg1/ColoredPoints.js
+++ asg1/ColoredPoints.js
@@ -97,15 +97,6 @@ function main() {
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
-class Point {
-  constructor() {
-    this.type='point';
-    this.position = [0.0, 0.0, 0.0];
-    this.color = [1.0,1.0,1.0,1.0];
-    this.size = 5.0;
-   }
-}
-
 var g_shapesList = [];
 
 //var g_points = [];  // The array for the position of a mouse press
@@ -158,24 +149,7 @@ function renderAllShapes() {
   var len = g_shapesList.length;  
 
   for(var i = 0; i < len; i++) {
-    //shape.render();
-
-    var xy = g_shapesList[i].position;
-    var rgba = g_shapesList[i].color;
-    var size = g_shapesList[i].size;
-    //var xy = g_points[i];
-    //var rgba = g_colors[i];
-    //var size = g_sizes[i];
-
-    // Pass the position of a point to a_Position variable
-    gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
-    // Pass the color of a point to u_FragColor variable
-    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-
-    gl.uniform1f(u_Size, size);    
-
-    // Draw
-    gl.drawArrays(gl.POINTS, 0, 1);
+    g_shapesList.render();
   }
   
 }
