227a7edeaa0a0557204030ce0b09b54e095ba6a0
Shanaya Malik <139424427+shanayamalik@users.noreply.github.com>
2024-04-08 14:05:51 -0700

    Update HelloTriangle.js

--- asg1/HelloTriangle.js
+++ asg1/HelloTriangle.js
@@ -47,10 +47,10 @@ function main() {
   //gl.drawArrays(gl.TRIANGLES, 0, n);
 }
 
-function initVertexBuffers(gl) {
-  var vertices = new Float32Array([
-    0, 0.5,   -0.5, -0.5,   0.5, -0.5
-  ]);
+function drawTriangle(vertices) {
+  //var vertices = new Float32Array([
+    //0, 0.5,   -0.5, -0.5,   0.5, -0.5
+  //]);
   var n = 3; // The number of vertices
 
   // Create a buffer object
@@ -63,7 +63,8 @@ function initVertexBuffers(gl) {
   // Bind the buffer object to target
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
   // Write date into the buffer object
-  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  //gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
   var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
   if (a_Position < 0) {
