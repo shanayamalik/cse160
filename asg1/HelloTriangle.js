adb4a359be3efaf0ce62be10f86f34e75c39c7b7
shanayamalik <shanaya.malik@gmail.com>
2024-04-08 14:49:59 -0700

    added more triangles to main function

--- asg1/HelloTriangle.js
+++ asg1/HelloTriangle.js
@@ -30,11 +30,11 @@ function main() {
   }
 
   // Write the positions of vertices to a vertex shader
-  var n = initVertexBuffers(gl);
-  if (n < 0) {
-    console.log('Failed to set the positions of the vertices');
-    return;
-  }
+  //var n = initVertexBuffers(gl);
+  //if (n < 0) {
+    //console.log('Failed to set the positions of the vertices');
+    //return;
+  //}
 
   // Specify the color for clearing <canvas>
   gl.clearColor(0, 0, 0, 1);
@@ -42,14 +42,17 @@ function main() {
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  // Draw the rectangle
-  gl.drawArrays(gl.TRIANGLES, 0, n);
+  // Draw the triangle
+  drawTriangle([0, 0.5,   -0.5, -0.5,   0.5, -0.5]);
+  drawTriangle([0.8, 0.9,   .7, .8,   .8, .7]);
+  drawTriangle([0.0, 0.0,   .5, 0,   .5, .5]);
+  //gl.drawArrays(gl.TRIANGLES, 0, n);
 }
 
-function initVertexBuffers(gl) {
-  var vertices = new Float32Array([
-    0, 0.5,   -0.5, -0.5,   0.5, -0.5
-  ]);
+function drawTriangle(vertices) {
+  //var vertices = new Float32Array([
+    //0, 0.5,   -0.5, -0.5,   0.5, -0.5
+  //]);
   var n = 3; // The number of vertices
 
   // Create a buffer object
@@ -62,7 +65,8 @@ function initVertexBuffers(gl) {
   // Bind the buffer object to target
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
   // Write date into the buffer object
-  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  //gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
   var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
   if (a_Position < 0) {
@@ -75,5 +79,6 @@ function initVertexBuffers(gl) {
   // Enable the assignment to a_Position variable
   gl.enableVertexAttribArray(a_Position);
 
-  return n;
+  gl.drawArrays(gl.TRIANGLES, 0, n);
+  //return n;
 }
