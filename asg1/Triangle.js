9ba00ed1322dda157f1817f359d57f3088d2412f
Shanaya Malik <139424427+shanayamalik@users.noreply.github.com>
2024-04-08 15:09:09 -0700

    Update Triangle.js

--- asg1/Triangle.js
+++ asg1/Triangle.js
@@ -1,3 +1,30 @@
+class Triangle {
+
+   constructor() {
+    this.type='triangle';
+    this.position = [0.0, 0.0, 0.0];
+    this.color = [1.0,1.0,1.0,1.0];
+    this.size = 5.0;
+   }
+
+   render() {
+    var xy = this.position;
+    var rgba = this.color; 
+    var size = this.size;
+    
+    // Pass the position of a point to a_Position variable
+    //gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
+    // Pass the color of a point to u_FragColor variable
+    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+    gl.uniform1f(u_Size, size);    
+
+   // Draw
+   //gl.drawArrays(gl.POINTS, 0, 1);
+   drawTriangle([xy[0], xy[1], xy[0]+.1, xy[1], xy[0], xy[1]+.1]);
+   }
+  
+}
+
 function drawTriangle(vertices) {
   //var vertices = new Float32Array([
     //0, 0.5,   -0.5, -0.5,   0.5, -0.5
@@ -17,11 +44,12 @@ function drawTriangle(vertices) {
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
   //gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
-  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-  if (a_Position < 0) {
-    console.log('Failed to get the storage location of a_Position');
-    return -1;
-  }
+  //var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  //if (a_Position < 0) {
+    //console.log('Failed to get the storage location of a_Position');
+    //return -1;
+  //}
+  
   // Assign the buffer object to a_Position variable
   gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
 
