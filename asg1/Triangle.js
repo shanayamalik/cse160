184009316291fa7e6e5e90014007cf6655f90f3b
Shanaya Malik <139424427+shanayamalik@users.noreply.github.com>
2024-04-08 22:52:01 -0700

    Update Triangle.js

--- asg1/Triangle.js
+++ asg1/Triangle.js
@@ -7,56 +7,43 @@ class Triangle {
     this.size = 5.0;
    }
 
-   render() {
+    render() {
     var xy = this.position;
     var rgba = this.color; 
     var size = this.size;
     
-    // Pass the position of a point to a_Position variable
-    //gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
     // Pass the color of a point to u_FragColor variable
     gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-    gl.uniform1f(u_Size, size);    
-
-   // Draw
-   //gl.drawArrays(gl.POINTS, 0, 1);
-   var d = this.size/200.0; //delta
-   drawTriangle([xy[0], xy[1], xy[0]+d, xy[1], xy[0], xy[1]+d]);
-   }
-  
+    
+    // Calculate the vertices of the triangle based on size and position
+    var d = this.size / 200.0; // delta
+    var vertices = [
+      xy[0], xy[1],
+      xy[0] + d, xy[1],
+      xy[0], xy[1] + d
+    ];
+    // Call drawTriangle to render the triangle
+    drawTriangle(vertices);
+  }
 }
 
+// The drawTriangle function should look like this
 function drawTriangle(vertices) {
-  //var vertices = new Float32Array([
-    //0, 0.5,   -0.5, -0.5,   0.5, -0.5
-  //]);
   var n = 3; // The number of vertices
-
-  // Create a buffer object
+  
+  // Create a buffer object, bind, and set data
   var vertexBuffer = gl.createBuffer();
   if (!vertexBuffer) {
     console.log('Failed to create the buffer object');
-    return -1;
+    return;
   }
-
-  // Bind the buffer object to target
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-  // Write date into the buffer object
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
-  //gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
-  //var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-  //if (a_Position < 0) {
-    //console.log('Failed to get the storage location of a_Position');
-    //return -1;
-  //}
-  
-  // Assign the buffer object to a_Position variable
+  // Set the vertex attribute pointer and enable it
   gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
-
-  // Enable the assignment to a_Position variable
   gl.enableVertexAttribArray(a_Position);
 
+  // Draw the triangle
   gl.drawArrays(gl.TRIANGLES, 0, n);
-  //return n;
 }
