42ca2a6faadeccbfe4a691b415d3c64d141d002d
shanayamalik <shanaya.malik@gmail.com>
2024-04-21 10:28:55 -0700

    updated generateVertices function to use part of render

--- lab2/src/Circle.js
+++ lab2/src/Circle.js
@@ -1,29 +1,21 @@
 export default class Circle {
   constructor() {
     this.type = "circle";
-    this.position = [0.0, 0.0, 0.0];
+    this.position = [0.0, 0.0, 0.0]; // Simplified to 2D
     this.color = [1.0, 1.0, 1.0, 1.0];
     this.size = 5.0;
     this.segments = 10;
-
     this.buffer = null;
+    this.vertices = null;
   }
 
-  render(gl) {
+  generateVertices() {
     let [x, y] = this.position;
-    let [r, g, b, a] = this.color;
-
-    const uFragColor = gl.getUniformLocation(gl.program, "uFragColor");
-    const aPosition = gl.getAttribLocation(gl.program, "aPosition");
-
-    // Pass the color of a point to uFragColor variable
-    gl.uniform4f(uFragColor, r, g, b, a);
-
-    // Draw
-    var d = this.size / 200.0; // delta
-
+    var d = this.size / 200.0; // delta for scaling
+    let v = [];
     let angleStep = 360 / this.segments;
-    for (var angle = 0; angle < 360; angle = angle + angleStep) {
+
+    for (var angle = 0; angle < 360; angle += angleStep) {
       let centerPt = [x, y];
       let angle1 = angle;
       let angle2 = angle + angleStep;
@@ -37,35 +29,33 @@ export default class Circle {
       ];
       let pt1 = [centerPt[0] + vec1[0], centerPt[1] + vec1[1]];
       let pt2 = [centerPt[0] + vec2[0], centerPt[1] + vec2[1]];
-
-      let vertices = [x, y, pt1[0], pt1[1], pt2[0], pt2[1]];
-
-      if (this.buffer === null) {
-        // Create a buffer object
-        this.buffer = gl.createBuffer();
-        if (!this.buffer) {
-          console.log("Failed to create the buffer object");
-          return -1;
-        }
-      }
-
-      // Bind the buffer object to target
-      gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
-
-      // Write data into the buffer object
-      gl.bufferData(
-        gl.ARRAY_BUFFER,
-        new Float32Array(vertices),
-        gl.DYNAMIC_DRAW
-      );
-
-      // Assign the buffer object to aPosition variable
-      gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
-
-      // Enable the assignment to aPosition variable
-      gl.enableVertexAttribArray(aPosition);
-
-      gl.drawArrays(gl.TRIANGLES, 0, 3);
+      v.push(x, y, pt1[0], pt1[1], pt2[0], pt2[1]);
     }
+    this.vertices = new Float32Array(v);
+  }
+
+  render(gl) {
+    if (this.vertices === null) {
+      this.generateVertices();
+    }
+
+    let uFragColor = gl.getUniformLocation(gl.program, "uFragColor");
+    let aPosition = gl.getAttribLocation(gl.program, "aPosition");
+
+    gl.uniform4f(uFragColor, ...this.color);
+
+    if (this.buffer === null) {
+      this.buffer = gl.createBuffer();
+      if (!this.buffer) {
+        console.log("Failed to create the buffer object");
+        return -1;
+      }
+    }
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, this.vertices, gl.DYNAMIC_DRAW);
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aPosition);
+    gl.drawArrays(gl.TRIANGLES, 0, this.vertices.length / 2); // Adjusted draw call
   }
 }
