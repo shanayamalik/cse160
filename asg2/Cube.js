1fe8536caf94b0c6470c5832f3ff9e26f25d674f
Shanaya Malik <139424427+shanayamalik@users.noreply.github.com>
2024-04-22 13:17:48 -0700

    Update Cube.js

--- asg2/Cube.js
+++ asg2/Cube.js
@@ -1,10 +1,11 @@
-class Circle {
+class Cube {
     constructor(segments) {
-        this.type = 'circle';
-        this.position = [0.0, 0.0, 0.0];
+        this.type = 'cube';
+        //this.position = [0.0, 0.0, 0.0];
         this.color = [1.0, 1.0, 1.0, 1.0];
-        this.size = 5.0;
-        this.segments = segments;
+        //this.size = 5.0;
+        //this.segments = segments;
+        this.matrix = new Matrix4();
     }
 
     render() {
@@ -16,22 +17,23 @@ class Circle {
         gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
 
         // Draw
-        var d = this.size/200.0; // delta
+        //var d = this.size/200.0; // delta
 
-        let angleStep = 360/this.segments;
-        for (var angle = 0; angle < 360; angle+=angleStep) {
-            let centerPt = [xy[0], xy[1]];
-            let angle1 = angle;
-            let angle2 = angle+angleStep;
-            let vec1 = [Math.cos(angle1*Math.PI/180)*d, Math.sin(angle1*Math.PI/180)*d];
-            let vec2 = [Math.cos(angle2*Math.PI/180)*d, Math.sin(angle2*Math.PI/180)*d];
-            let pt1 = [centerPt[0]+vec1[0], centerPt[1]+vec1[1]];
-            let pt2 = [centerPt[0]+vec2[0], centerPt[1]+vec2[1]];
+        //let angleStep = 360/this.segments;
+        //for (var angle = 0; angle < 360; angle+=angleStep) {
+            //let centerPt = [xy[0], xy[1]];
+            //let angle1 = angle;
+            //let angle2 = angle+angleStep;
+            //let vec1 = [Math.cos(angle1*Math.PI/180)*d, Math.sin(angle1*Math.PI/180)*d];
+            //let vec2 = [Math.cos(angle2*Math.PI/180)*d, Math.sin(angle2*Math.PI/180)*d];
+            //let pt1 = [centerPt[0]+vec1[0], centerPt[1]+vec1[1]];
+            //let pt2 = [centerPt[0]+vec2[0], centerPt[1]+vec2[1]];
 
-            drawTriangle([xy[0], xy[1], pt1[0], pt1[1], pt2[0], pt2[1]]);
+            //drawTriangle([xy[0], xy[1], pt1[0], pt1[1], pt2[0], pt2[1]]);
+
+        drawTriangle3D([0.0,0.0,0.0,  1.0,1.0,0.0,  1.0,0.0,0.0]);
+        drawTriangle3D([0.0,0.0,0.0,  0.0,1.0,0.0,  1.0,1.0,0.0]);
           
         }
       
     }
-  
-}
