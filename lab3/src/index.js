26b6f7669f3f588c884edd9c72ccede175bb8de7
Shanaya Malik <139424427+shanayamalik@users.noreply.github.com>
2024-04-21 22:28:34 -0700

    Update index.js

--- lab3/src/index.js
+++ lab3/src/index.js
@@ -14,6 +14,7 @@ import Cube from "./Cube";
 import Camera from "./Camera";
 import RotateControls from "./Controls";
 import uvImg from "./img/uvCoords.png";
+import diceImg from "./img/dice.png";
 
 const stats = new Stats();
 document.body.append(stats.dom);
