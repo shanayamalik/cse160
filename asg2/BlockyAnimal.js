6bc07e885f0aadfd907f9683c936450c7337afde
Shanaya Malik <139424427+shanayamalik@users.noreply.github.com>
2024-04-22 21:50:02 -0700

    Update BlockyAnimal.js

--- asg2/BlockyAnimal.js
+++ asg2/BlockyAnimal.js
@@ -30,7 +30,6 @@ function setupWebGL() {
   canvas = document.getElementById('webgl');
 
   // Get the rendering context for WebGL
-  //gl = getWebGLContext(canvas);
   gl = canvas.getContext("webgl", { preserveDrawingBuffer: true});
   if (!gl) {
     console.log('Failed to get the rendering context for WebGL');
@@ -46,7 +45,7 @@ function connectVariablesToGLSL() {
     return;
   }
 
-  // // Get the storage location of a_Position
+  // Get the storage location of a_Position
   a_Position = gl.getAttribLocation(gl.program, 'a_Position');
   if (a_Position < 0) {
     console.log('Failed to get the storage location of a_Position');
@@ -78,49 +77,32 @@ function connectVariablesToGLSL() {
 }
 
 // Constants
-const POINT = 0;
-const TRIANGLE = 1;
-const CIRCLE = 2;
+//const POINT = 0;
+//const TRIANGLE = 1;
+//const CIRCLE = 2;
 
 // Global Variables Related to UI Elements
 let g_selectedColor=[1.0,1.0,1.0,1.0];
 let g_selectedSize=5;
-let g_selectedType=POINT;
+//let g_selectedType=POINT;
 let g_globalAngle=0; 
 let g_yellowAngle=0;
-//let g_selectedType=TRIANGLE;
+let g_magentaAngle=0;
+let g_yellowAnimation=false;
+let g_magentaAnimation=false;
+let g_seconds=0;
+//let g_startTime=0;
 
 function addActionsForHtmlUI() {
-  // Button Events
-  //document.getElementById('green').onclick = function() {g_selectedColor = [0.0,1.0,0.0,1.0];
-    //document.getElementById('greenSlide').value=100;
-    //document.getElementById('redSlide').value=0;
-    //document.getElementById('blueSlide').value=0;
- //};
-  //document.getElementById('red').onclick = function() {g_selectedColor = [1.0,0.0,0.0,1.0];
-    //document.getElementById('greenSlide').value=0;
-    //document.getElementById('redSlide').value=100;
-    //document.getElementById('blueSlide').value=0;
-  //};
-  //document.getElementById('blue').onclick = function() {g_selectedColor = [0.0,0.0,1.0,1.0];
-    //document.getElementById('greenSlide').value=0;
-    //document.getElementById('redSlide').value=0;
-    //document.getElementById('blueSlide').value=100;
-  //};  
-
-  //document.getElementById('clearButton').onclick = function() {gl.clear(gl.COLOR_BUFFER_BIT); g_shapesList = []; renderAllShapes(); };
-
-  //document.getElementById('pointButton').onclick = function() {g_selectedType=POINT};
-  //document.getElementById('triButton').onclick = function() {g_selectedType=TRIANGLE};
-  //document.getElementById('circleButton').onclick = function() {g_selectedType=CIRCLE};
+document.getElementById('yellowSlide').addEventListener('mousemove', function() {g_yellowAngle = this.value; renderAllShapes(); });
+  document.getElementById('magentaSlide').addEventListener('mousemove', function() {g_magentaAngle = this.value; renderAllShapes(); });
   
-  //document.getElementById('redSlide').addEventListener('mouseup', function() {g_selectedColor[0] = this.value/100; });
-  //document.getElementById('greenSlide').addEventListener('mouseup', function() {g_selectedColor[1] = this.value/100; });  
-  //document.getElementById('blueSlide').addEventListener('mouseup', function() {g_selectedColor[2] = this.value/100; });
+document.getElementById('animationYellowOnButton').onclick = function() {g_yellowAnimation=true;};
+document.getElementById('animationYellowOffButton').onclick = function() {g_yellowAnimation=false;};
 
-  document.getElementById('yellowSlide').addEventListener('mousemove', function() {g_yellowAngle = this.value; renderAllShapes(); });
+document.getElementById('animationMagentaOnButton').onclick = function() {g_magentaAnimation=true;};
+document.getElementById('animationMagentaOffButton').onclick = function() {g_magentaAnimation=false;};
   document.getElementById('angleSlide').addEventListener('mousemove', function() {g_globalAngle = this.value; renderAllShapes(); });
-
 }
 
 function main() {
@@ -129,20 +111,14 @@ function main() {
 
   // Set up actions for HTML UI 
   addActionsForHtmlUI();
-
-  // Register function (event handler) to be called on a mouse press
-  canvas.onmousedown = click;
-  //canvas.onmousemove = click;
-  canvas.onmousemove = function(ev) { if(ev.buttons == 1) { click(ev) } };
   
   // Specify the color for clearing <canvas>
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
-  renderAllShapes();
+  g_startTime=performance.now()/1000.0;
+  //renderAllShapes();
+  requestAnimationFrame(tick);
   
 }
-
-var g_shapesList = [];
 
 function click(ev) {
   let [x, y] = convertCoordinatesEventToGL(ev);
@@ -156,7 +132,7 @@ function click(ev) {
   } else if (g_selectedType == CIRCLE) {
     point = new Circle(g_selectedSegments); 
   }
-  
+
   point.position = [x,y];
   point.color = g_selectedColor.slice();
   point.size = g_selectedSize;
@@ -165,15 +141,28 @@ function click(ev) {
   renderAllShapes();
 }
 
-function convertCoordinatesEventToGL(ev) {
-  var x = ev.clientX; // x coordinate of a mouse pointer
-  var y = ev.clientY; // y coordinate of a mouse pointer
-  var rect = ev.target.getBoundingClientRect();
+// Called by browser repeatedly whenever its time
+function tick() {
+    g_seconds=performance.now()/1000.0-g_startTime;
+    // Print some debug information so we know we are running
+    //console.log(performance.now());
 
-  x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
-  y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
+    updateAnimationAngles();
+  
+    // Draw everything
+    renderAllShapes();
 
-  return([x,y]);
+    // Tell the browser to update again when it has time
+    requestAnimationFrame(tick);
+}
+
+function updateAnimationAngles() {
+    if (g_yellowAnimation) {
+        g_yellowAngle = (45 * Math.sin(g_seconds));
+    }
+    if (g_magentaAnimation) {
+        g_magentaAngle = (45 * Math.sin(3 * g_seconds));
+    }
 }
 
 function renderAllShapes() {
@@ -187,29 +176,43 @@ function renderAllShapes() {
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   //gl.clear(gl.COLOR_BUFFER_BIT);
 
-  //drawTriangle3D([-1.0,0.0,0.0,  -0.5,-1.0,0.0,   0.0,0.0,0.0]);
-
-  //Draw a cube
+  // Draw the body cube
   var body = new Cube();
   body.color = [1.0, 0.0, 0.0, 1.0];
-  body.matrix.translate(-0.25, -0.5, 0.0);
-  body.matrix.scale(0.5, 1, 0.5);
+  body.matrix.translate(-0.25, -0.75, 0.0);
+  body.matrix.rotate(-5,1,0,0);
+  body.matrix.scale(0.5, .3, .5);
   body.render();
 
-  //Draw a left arm 
-  var leftArm = new Cube();
-  leftArm.color = [1, 1, 0, 1];
-  leftArm.matrix.setTranslate(0.7, 0, 0.0);
-  leftArm.matrix.rotate(-g_yellowAngle, 0, 0, 1);
-  leftArm.matrix.scale(0.25, 0.7, 0.5);
-  leftArm.render();
+  // Draw a left arm
+  var yellow = new Cube();
+  yellow.color = [1, 1, 0, 1];
+  yellow.matrix.setTranslate(0, -0.5, 0.0); 
+  yellow.matrix.rotate(-5,1,0,0);
+  yellow.matrix.rotate(-g_yellowAngle,0,0,1);
+  
+  //if (g_yellowAnimation) {
+      //yellow.matrix.rotate(45*Math.sin(g_seconds), 0,0,1);
+  //} else {
+      //yellow.matrix.rotate(-g_yellowAngle, 0,0,1);
+  //}
+  
+  var yellowCoordinatesMat=new Matrix4(yellow.matrix);
+  yellow.matrix.scale(0.25, 0.7, 0.5);
+  yellow.matrix.translate(-0.5,0,0);
+  yellow.render();
+
 
   // Test box
   var box = new Cube();
   box.color = [1, 0, 1, 1];
-  box.matrix.translate(0, 0, -0.50, 0);
-  box.matrix.rotate(-30, 1, 0, 0);
-  box.matrix.scale(.5, .5, .5);
+  box.matrix = yellowCoordinatesMat;
+  box.matrix.translate(0, 0.65, 0);
+  box.matrix.rotate(g_magentaAngle, 0, 0, 1);
+  box.matrix.scale(0.3, 0.3, 0.3);
+  box.matrix.translate(-0.5, 0, -0.001);
+  //box.matrix.rotate(-30, 1, 0, 0);
+  //box.matrix.scale(.5, .5, .5);
   box.render();
   
   var duration = performance.now() - StartTime;
@@ -224,5 +227,5 @@ function sendTextToHTML(text, htmlID) {
       console.log("Failed to get " + htmlID + " from HTML");
       return;
   }
-htmlElm. innerHTML = text;
+htmlElm.innerHTML = text;
 }
