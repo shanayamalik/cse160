9b6ae9a4d7ace21b9d61103248d770dbbcb10183
Shanaya Malik <139424427+shanayamalik@users.noreply.github.com>
2024-04-22 18:24:02 -0700

    Update Cube.js

--- asg2/Cube.js
+++ asg2/Cube.js
@@ -8,20 +8,44 @@ class Cube {
         this.matrix = new Matrix4();
     }
 
-    render() {
-        //var xy = this.position;
-        var rgba = this.color;
-        //var size = this.size;
+  render() {
+    //var xyz = this.position;
+    var rgba = this.color;
+    //var size = this.size;
 
-        // Pass the color of a point to u_FragColor variable
-        gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+    // Pass the color of a point to u_FragColor uniform variable
+    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
 
-        console.log(this.matrix.elements);
-      
-        gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
+    // Pass the matrix to u_ModelMatrix attribute
+    gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
 
-        drawTriangle3D([0.0,0.0,0.0,  1.0,1.0,0.0,  1.0,0.0,0.0]);
-        drawTriangle3D([0.0,0.0,0.0,  0.0,1.0,0.0,  1.0,1.0,0.0]);
-        // Include other five sides of triangle
-    } 
-}
+    // Front of cube
+    drawTriangle3D( [0,0,0, 1,1,0, 1,0,0] );
+    //drawTriangle3D( [0,0,1, 1,1,1, 1,0,1] );
+    //drawTriangle3D( [0,0,0, 0,1,0, 0,1,1, 0,0,1] );
+    //drawTriangle3D( [0,0,0, 0,1,0, 0,1,1, 0,0,1] );
+    //drawTriangle3D( [0,0,0, 1,1,0, 1,1,1, 0,0,1] );
+    drawTriangle3D( [0,0,0, 0,1,0, 1,1,0] );
+    //drawTriangle3D( [0,0,0, 0,1,1, 1,1,1] );
+
+    // Change the color to white for the top of the cube
+    gl.uniform4f(u_FragColor, rgba[0]*.9, rgba[1]*.9, rgba[2]*.9, rgba[3]);
+
+    // Top of cube
+    drawTriangle3D( [0,1,0, 0,1,1, 1,1,1] );
+    drawTriangle3D( [0,1,0, 1,1,1, 1,1,0] );
+
+    // Back of cube
+    drawTriangle3D([1,0,1, 0,1,1, 0,0,1]);
+    drawTriangle3D([1,0,1, 1,1,1, 0,1,1]);
+    // Bottom of cube
+    drawTriangle3D([0,0,0, 1,0,1, 1,0,0]);
+    drawTriangle3D([0,0,0, 0,0,1, 1,0,1]);
+    // Left side of cube
+    drawTriangle3D([0,0,0, 0,0,1, 0,1,1]);
+    drawTriangle3D([0,0,0, 0,1,1, 0,1,0]);
+    // Right side of cube
+    drawTriangle3D([1,0,0, 1,1,1, 1,1,0]);
+    drawTriangle3D([1,0,0, 1,0,1, 1,1,1]);
+  } 
+    }
