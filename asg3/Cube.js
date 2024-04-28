0144ddf05ce923d17902c2acbddec5521e2bc834
Shanaya Malik <139424427+shanayamalik@users.noreply.github.com>
2024-04-28 13:22:08 -0700

    Update Cube.js

--- asg3/Cube.js
+++ asg3/Cube.js
@@ -15,7 +15,7 @@ class Cube {
     gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
 
     // Front of cube
-    //drawTriangle3D( [0,0,0, 1,1,0, 1,0,0] );
+    drawTriangle3D( [0,0,0, 1,1,0, 1,0,0] );
     drawTriangle3DUV([0,0,0 , 1,1,0 , 1,0,0 ], [1,0 , 0,1 , 1,1]);
     drawTriangle3D( [0,0,0, 0,1,0, 1,1,0] );
 
