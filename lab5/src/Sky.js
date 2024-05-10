d2c6c0403ed52a3f46858487b287d91d91152995
shanayamalik <shanaya.malik@gmail.com>
2024-05-09 20:05:36 -0700

    defined position for sun

--- lab5/src/Sky.js
+++ lab5/src/Sky.js
@@ -1,7 +1,30 @@
-import Sphere from "./primitives/Sphere";
-import { VertexShader, FragmentShader } from "./shaders/Sky";
+const VertexShader = `
+  precision mediump float;
+  attribute vec3 position;
+  attribute vec2 uv;
+  attribute vec3 normal;
 
-const Sky = new Sphere(50, 20, 20); // radius 50, 20x20 resolution
-Sky.setShaders(VertexShader, FragmentShader);
+  uniform mat4 modelMatrix;
+  uniform mat4 normalMatrix;
+  uniform mat4 viewMatrix;
+  uniform mat4 projectionMatrix;
 
-export default Sky;
+  void main() {
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    gl_Position = projectionMatrix * viewMatrix * modelPosition;
+  }
+`;
+
+const FragmentShader = `
+  precision mediump float;
+  #define SUN_POS vec3(0.0, 25.0, 43.301)
+  
+  void main() {
+    vec3 color = vec3(0.1, 0.12, 0.4);
+
+    gl_FragColor = vec4(color, 1.0);
+  }
+`;
+
+export { VertexShader, FragmentShader };
