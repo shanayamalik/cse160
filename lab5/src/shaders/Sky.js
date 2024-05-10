731dfaad5486d451d91da7f8519b26306b04cb2e
shanayamalik <shanaya.malik@gmail.com>
2024-05-09 19:59:20 -0700

    created directory for lab5

---
+++ lab5/src/shaders/Sky.js
@@ -0,0 +1,29 @@
+const VertexShader = `
+  precision mediump float;
+  attribute vec3 position;
+  attribute vec2 uv;
+  attribute vec3 normal;
+
+  uniform mat4 modelMatrix;
+  uniform mat4 normalMatrix;
+  uniform mat4 viewMatrix;
+  uniform mat4 projectionMatrix;
+
+  void main() {
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    gl_Position = projectionMatrix * viewMatrix * modelPosition;
+  }
+`;
+
+const FragmentShader = `
+  precision mediump float;
+
+  void main() {
+    vec3 color = vec3(0.1, 0.12, 0.4);
+
+    gl_FragColor = vec4(color, 1.0);
+  }
+`;
+
+export { VertexShader, FragmentShader };
