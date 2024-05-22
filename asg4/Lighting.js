3e9092c84eb4b9eecb9ea2358ce0e8ab1a7d9964
shanayamalik <shanaya.malik@gmail.com>
2024-05-21 22:13:00 -0700

    light

--- asg4/Lighting.js
+++ asg4/Lighting.js
@@ -16,6 +16,7 @@ var VSHADER_SOURCE = `
   attribute vec3 a_Normal;
   varying vec2 v_UV;
   varying vec3 v_Normal;
+  varying vec4 v_VertPos;
   attribute vec4 a_Color;
   uniform mat4 u_ModelMatrix;
   uniform mat4 u_GlobalRotateMatrix;
@@ -25,6 +26,7 @@ var VSHADER_SOURCE = `
     gl_Position = u_ProjectionMatrix * u_ViewMatrix * u_GlobalRotateMatrix * u_ModelMatrix * a_Position;
     v_UV = a_UV;
     v_Normal = a_Normal;
+    v_VertPos = u_ModelMatrix * a_Position;
   }`
 
 // Fragment shader program
@@ -41,6 +43,8 @@ var FSHADER_SOURCE = `
   uniform sampler2D u_Sampler5;  // Existing sampler for the sunrise sky texture
   uniform sampler2D u_Sampler6;  // New sampler for the additional texture
   uniform int u_whichTexture;
+  uniform vec3 u_lightPos;
+  varying vec4 v_VertPos;
 
   void main() {
     if (u_whichTexture == -3) {
@@ -66,6 +70,15 @@ var FSHADER_SOURCE = `
     } else {
       gl_FragColor = vec4(1.0, 0.2, 0.2, 1.0); // Fallback color for errors
     }
+
+    vec3 lightVector = vec3(v_VertPos) - u_lightPos;
+    float r = length(lightVector);
+    if (r < 0.0) {
+        gl_FragColor = vec4(1, 0, 0, 1);
+    } else if (r < 0.0) {
+        gl_FragColor = vec4(0, 1, 0, 1);
+    }
+
   }`;
 
 // Global Variables (UI or data passed to GLSL)
@@ -82,6 +95,7 @@ let u_ProjectionMatrix;
 let u_ViewMatrix;
 let u_Samplers = [];
 let u_whichTexture;
+let u_lightPos;
 let g_horizontalAngle = 175.0;
 let g_verticalAngle = 0.0;
 //let isNightMode = false;
@@ -95,8 +109,9 @@ let g_yellowAngle=0;
 let g_magentaAngle=0;
 let g_yellowAnimation=false;
 let g_magentaAnimation=false;
+let g_NormalOn=false;
 
-let g_lightPos=0;
+let g_lightPos=[0, 1, -2];
 
 const setupWebGL = () => {
   // Retrieve <canvas> element
@@ -151,6 +166,13 @@ const connectVariablesToGLSL = () => {
     return;
   }
 
+  // Get the storage location of u_lightPos
+  u_lightPos = gl.getUniformLocation(gl.program, 'u_lightPos');
+  if (!u_lightPos) {
+      console.log('Failed to get the storage location of u_lightPos');
+      return;
+  }
+  
   // Get the storage location of u_ModelMatrix
   u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
   if (!u_ModelMatrix) {
@@ -264,14 +286,18 @@ const addActionsForHtmlUI = () => {
       dragging = false;
   });
 
+document.getElementById('normalOn').onclick = function() { g_normalOn = true; };
+document.getElementById('normalOff').onclick = function() { g_normalOn = false; };
   document.getElementById('yellowSlide').addEventListener('mousemove', function() {g_yellowAngle = this.value; renderAllShapes(); });
-  document.getElementById('magentaSlide').addEventListener('mousemove', function() {g_magentaAngle = this.value; renderAllShapes(); });
+document.getElementById('magentaSlide').addEventListener('mousemove', function() {g_magentaAngle = this.value; renderAllShapes(); });
+  document.getElementById('lightSlideX').addEventListener('mousemove', function(ev) { if (ev.buttons == 1) { g_lightPos[0] = this.value / 100; renderAllShapes(); } });
+  document.getElementById('lightSlideY').addEventListener('mousemove', function(ev) { if (ev.buttons == 1) { g_lightPos[1] = this.value / 100; renderAllShapes(); } });
+  document.getElementById('lightSlideZ').addEventListener('mousemove', function(ev) { if (ev.buttons == 1) { g_lightPos[2] = this.value / 100; renderAllShapes(); } });
   
-  document.getElementById('animationYellowOnButton').onclick = function() {g_yellowAnimation=true;};
-  document.getElementById('animationYellowOffButton').onclick = function() {g_yellowAnimation=false;};
-  
-  document.getElementById('animationMagentaOnButton').onclick = function() {g_magentaAnimation=true;};
-  document.getElementById('animationMagentaOffButton').onclick = function() {g_magentaAnimation=false;};
+document.getElementById('animationYellowOnButton').onclick = function() {g_yellowAnimation=true;};
+document.getElementById('animationYellowOffButton').onclick = function() {g_yellowAnimation=false;};
+document.getElementById('animationMagentaOnButton').onclick = function() {g_magentaAnimation=true;};
+document.getElementById('animationMagentaOffButton').onclick = function() {g_magentaAnimation=false;};
   
 }
 
@@ -509,7 +535,7 @@ function renderDaySky() {
   const sky = new Cube();
   sky.color = [1,1,1,1];
   sky.textureNum = 1;
-  sky.matrix.scale(128, 128, 128);
+  sky.matrix.scale(-128, -128, -128);
   sky.matrix.translate(-0.5, -0.5, -0.5);
   sky.render();
 } 
@@ -518,7 +544,7 @@ function renderNightSky() {
   const night_sky = new Cube();
   //night_sky.color = [1,1,1,1];
   night_sky.textureNum = 4;
-  night_sky.matrix.scale(128, 128, 128);
+  night_sky.matrix.scale(-128, -128, -128);
   night_sky.matrix.translate(-0.5, -0.5, -0.5);
   gl.uniform1i(u_whichTexture, 4);
   night_sky.render();
@@ -528,16 +554,38 @@ function renderSunriseSky() {
     const night_sky = new Cube();
     //night_sky.color = [1,1,1,1];
     night_sky.textureNum = 5;
-    night_sky.matrix.scale(128, 128, 128);
+    night_sky.matrix.scale(-128, -128, -128);
     night_sky.matrix.translate(-0.5, -0.5, -0.5);
     gl.uniform1i(u_whichTexture, 5);
     night_sky.render();
   }
 */
+
+  /*
+  // Pass the light position to GLSL
+  gl.uniform3f(u_lightPos, g_lightPos[0], g_lightPos[1], g_lightPos[2]);
+
+  // Draw the light
+  var light = new Cube();
+  light.color = [2, 2, 0, 1];
+  light.matrix.translate(g_lightPos[0], g_lightPos[1], g_lightPos[2]);
+  light.matrix.scale(.1, .1, .1);
+  light.matrix.translate(-.5, -.5, -.5);
+  light.render();
+
+  // Draw Sphere
+  var sp = new Sphere();
+  //sp.textureNum = -1;
+  if (g_normalOn) sp.textureNum = -3;
+  sp.matrix.translate(-1, -1.5, -1.5);
+  //sp.matrix.scale(.4, .4, .4);
+  sp.render();
+  */
   
   // Draw the body cube
   var body = new Cube();
   body.color = [1.0, 0.0, 0.0, 1.0];
+  //if (g_NormalOn) body.textureNum = -3;
   body.textureNum = -2;
   body.matrix.translate(-0.25, -0.75, 0.0);
   body.matrix.rotate(-5,1,0,0);
@@ -547,6 +595,7 @@ function renderSunriseSky() {
   // Draw a left arm
   var yellow = new Cube();
   yellow.color = [1, 1, 0, 1];
+  //if (g_NormalOn) yellow.textureNum = -2;
   yellow.textureNum = -2;
   yellow.matrix.setTranslate(0, -0.5, 0.0); 
   yellow.matrix.rotate(-5,1,0,0);
@@ -559,6 +608,7 @@ function renderSunriseSky() {
   // Test box
   var box = new Cube();
   box.color = [1, 0, 1, 1];
+  //if (g_NormalOn) box.textureNum = -3;
   box.textureNum = -2;
   box.matrix = yellowCoordinatesMat;
   box.matrix.translate(0, 0.65, 0);
