522fc454d347cce580f3505c989912212bfee902
shanayamalik <shanaya.malik@gmail.com>
2024-05-21 20:56:26 -0700

    commented DrawTriangle3DUVNormal

--- asg4/Cube.js
+++ asg4/Cube.js
@@ -100,39 +100,30 @@ class Cube {
     }
 
     /*
-      render() {
-        var rgba = this.color;
+    // Top of cube
+    drawTriangle3DUVNormal([0,1,0, 0,1,1, 1,1,1], [0,1, 0,0, 1,0]);
+    drawTriangle3DUVNormal([0,1,0, 1,1,1, 1,1,0], [0,1, 1,0, 1,1]);
 
-        // Pass the color of a point to u_FragColor uniform variable
-        gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+    // Back of cube
+    gl.uniform4f(u_FragColor, rgba[0]*.8, rgba[1]*.8, rgba[2]*.8, rgba[3]);
+    drawTriangle3DUVNormal([1,0,1, 0,1,1, 0,0,1], [1,0, 0,1, 0,0]);
+    drawTriangle3DUVNormal([1,0,1, 1,1,1, 0,1,1], [1,0, 1,1, 0,1]);
 
-        // Pass the matrix to u_ModelMatrix attribute
-        gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
+    // Bottom of cube
+    gl.uniform4f(u_FragColor, rgba[0]*.7, rgba[1]*.7, rgba[2]*.7, rgba[3]);
+    drawTriangle3DUVNormal([0,0,0, 1,0,1, 1,0,0], [0,0, 1,0, 1,0]);
+    drawTriangle3DUVNormal([0,0,0, 0,0,1, 1,0,1], [0,0, 0,0, 1,0]);
 
-        // Front of cube
-        drawTriangle3D( [0,0,0, 1,1,0, 1,0,0] );
-        drawTriangle3D( [0,0,0, 0,1,0, 1,1,0] );
+    // Left side of cube
+    gl.uniform4f(u_FragColor, rgba[0]*.6, rgba[1]*.6, rgba[2]*.6, rgba[3]);
+    drawTriangle3DUVNormal([0,0,0, 0,0,1, 0,1,1], [0,0, 0,1, 0,1]);
+    drawTriangle3DUVNormal([0,0,0, 0,1,1, 0,1,0], [0,0, 0,1, 0,0]);
 
-        // Change the color to white for the top of the cube
-        gl.uniform4f(u_FragColor, rgba[0]*.9, rgba[1]*.9, rgba[2]*.9, rgba[3]);
-
-        // Top of cube
-        drawTriangle3D( [0,1,0, 0,1,1, 1,1,1] );
-        drawTriangle3D( [0,1,0, 1,1,1, 1,1,0] );
-        // Back of cube
-        drawTriangle3D([1,0,1, 0,1,1, 0,0,1]);
-        drawTriangle3D([1,0,1, 1,1,1, 0,1,1]);
-        // Bottom of cube
-        drawTriangle3D([0,0,0, 1,0,1, 1,0,0]);
-        drawTriangle3D([0,0,0, 0,0,1, 1,0,1]);
-        // Left side of cube
-        drawTriangle3D([0,0,0, 0,0,1, 0,1,1]);
-        drawTriangle3D([0,0,0, 0,1,1, 0,1,0]);
-        // Right side of cube
-        drawTriangle3D([1,0,0, 1,1,1, 1,1,0]);
-        drawTriangle3D([1,0,0, 1,0,1, 1,1,1]);
-      }
-    */
+    // Right side of cube
+    gl.uniform4f(u_FragColor, rgba[0]*.5, rgba[1]*.5, rgba[2]*.5, rgba[3]);
+    drawTriangle3DUVNormal([1,0,0, 1,1,1, 1,1,0], [1,0, 1,1, 1,1]);
+    drawTriangle3DUVNormal([1,0,0, 1,0,1, 1,1,1], [1,0, 1,0, 1,1]);
+*/
     
     renderFast() {
         let rgba = this.color;
