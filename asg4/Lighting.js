232a070c26f4557d3ea09058556270de7c8db8d4
shanayamalik <shanaya.malik@gmail.com>
2024-05-25 17:39:56 -0700

    added global variables for spotlight and removed commented blocky animal

--- asg4/Lighting.js
+++ asg4/Lighting.js
@@ -131,7 +131,7 @@ void main() {
     }
 }`;
 
-// Global Variables (UI or data passed to GLSL)
+// Global Variables
 let canvas;
 let gl;
 let a_Position;
@@ -154,11 +154,6 @@ let isLightPosSliderActive = false;
 //let isNightMode = false;
 //let isSunriseMode = false;
 
-let u_spotLightPos;
-let u_spotLightDir;
-let u_spotLightCutoff;
-let u_spotLightOuterCutoff;
-
 // Global Variables Related to UI Elements
 let g_selectedColor=[1.0,1.0,1.0,1.0];
 let g_selectedSize=5;
@@ -170,6 +165,16 @@ let g_magentaAnimation=false;
 let g_NormalOn=false;
 let g_lightOn=true;
 let g_lightPos=[0, 1, -2];
+
+// Global Variables for Spotlight
+let u_spotLightPos;
+let u_spotLightDir;
+let u_spotLightCutoff;
+let u_spotLightOuterCutoff;
+let spotlightPosition = [1.0, 2.0, 2.0];
+let spotlightDirection = [0.0, -1.0, -1.0];
+let spotlightCutoff = Math.cos(radians(12.5));
+let spotlightOuterCutoff = Math.cos(radians(17.5));
 
 function radians(degrees) {
     return degrees * Math.PI / 180.0;
@@ -754,133 +759,6 @@ function renderSunriseSky() {
   sp.matrix.translate(0, 0, -1.5); // Adjust translation to move sphere into view
   sp.matrix.scale(0.15, 0.15, 0.15); // Adjust scale to appropriate size
   sp.render();
-
-/*
-      // Draw the body cube
-      var body = new Cube();
-      body.color = [0.776, 0.525, 0.259, 1.0];
-      body.textureNum = g_NormalOn ? -3 : -2;
-      //body.color = g_color_3;
-      //ear.color = g_color_1;
-      body.matrix.setTranslate(-0.25, -0.25, 0.0);
-      var bodyCoordinatesMat=new Matrix4(body.matrix);
-      body.matrix.scale(0.5, .5, .75);
-      body.render();
-
-      // Draw the tail
-      var tail = new Cube();
-      tail.textureNum = g_NormalOn ? -3 : -2;
-      //tail.color = [1, 0.65, 0.65, 1.0];
-      tail.matrix = cloneMatrix4(bodyCoordinatesMat);
-      tail.matrix.translate(0.075, 0.10, 0.75);
-      tail.matrix.scale(0.35, 0.5, 0.5);
-
-      // Draw the tail
-      var tail2 = new Cube();
-      tail2.textureNum = g_NormalOn ? -3 : -2;
-      //tail2.color =[0.945, 0.761, 0.490, 1.0];
-      tail2.matrix = bodyCoordinatesMat;
-      tail2.matrix.translate(0.075, 0.10, 0.75);
-      tail2.matrix.rotate(-g_tailAngle,1,0,0);
-      tail2.matrix.scale(0.15, 0.55, 0.15);
-      tail2.render();
-
-      // Draw a neck
-      var neck = new Cube();
-      neck.textureNum = g_NormalOn ? -3 : -2;
-      //neck.color = [0.992, 0.961, 0.886, 1.0];
-      neck.matrix = bodyCoordinatesMat;
-      neck.matrix.setTranslate(0.0, 0.10, 0.05); 
-      neck.matrix.rotate(-g_neckAngle,1,0,0);
-      var neckCoordinatesMat=new Matrix4(neck.matrix);
-      neck.matrix.scale(0.25, 0.45, 0.25);
-      neck.matrix.translate(-0.5,0,0);
-      neck.render();
-
-      // Draw a head
-      var head = new Cube();
-      head.color = [0.945, 0.761, 0.490, 1.0];
-      head.matrix = neckCoordinatesMat;
-      head.matrix.translate(0, 0.45, -0.15);
-      head.matrix.rotate(g_headAngle*0.5, 0, 1, 0);
-      var headCoordinatesMat=new Matrix4(head.matrix);
-      head.matrix.scale(0.35, 0.3, 0.45);
-      head.matrix.translate(-0.5, 0, -0.0001);
-      head.render();
-
-      // Draw a nose
-      var nose = new Cube();
-      nose.color = [0.35, 0.35, 0.35, 1.0];
-      nose.matrix = cloneMatrix4(headCoordinatesMat);
-      nose.matrix.translate(0, 0.45, -0.10);
-      if (g_nose_size === 1)
-        nose.matrix.scale(0.15, 0.10, 0.25);
-      else
-       nose.matrix.scale(0.10, 0.10, 0.10);
-      nose.matrix.translate(-0.5, -4.25, -0.0001);
-      nose.render();
-
-      // Draw a nose
-      var nose2 = new Cube();
-      nose.color = [0.35, 0.35, 0.35, 1.0];
-      nose2.matrix = headCoordinatesMat;
-      nose2.matrix.translate(0, 0.45, -0.10);
-      nose2.matrix.scale(0.15, 0.10, 0.25);
-      nose2.matrix.translate(-0.5, -4.25, -0.0001);
-
-      // Draw ears
-      var ear = new Tetrahedron(); // Left ear
-      ear.color = g_color_1;
-      ear.matrix = neckCoordinatesMat;
-      ear.matrix = headCoordinatesMat;
-      ear.matrix.translate(1.0, 2.65, -0.20);
-      ear.matrix.scale(0.4, 1.5, 0.85);
-      ear.matrix.rotate(-g_earsAngle,1,0,0);
-      //var earsCoordinatesMat=new Matrix4(ears.matrix);
-      ear.render();
-
-      var ear2 = new Tetrahedron(); // Right ear
-      ear2.color = g_color_1;
-      ear2.matrix = headCoordinatesMat;
-      ear2.matrix.translate(-2.5, 0, -0.10);
-      ear2.matrix.scale(0.8, 1.0, 0.85);
-      ear2.matrix.rotate(-g_earsAngle,1,0,0);  
-      //var earsCoordinatesMat=new Matrix4(ears.matrix);
-      ear2.render();
-
-      // Draw four legs
-      var leg = new Cube(); // Front left
-      leg.color = [0.992, 0.961, 0.886, 1.0];
-      leg.matrix = bodyCoordinatesMat;
-      leg.matrix.setTranslate(0.05, -0.2, 0.05); 
-      leg.matrix.rotate(g_legsAngle,1,0,0);
-      leg.matrix.scale(0.05, -0.45, 0.05);
-      leg.render();
-
-      var leg2 = new Cube(); // Front right
-      leg2.color = [0.992, 0.961, 0.886, 1.0];
-      leg2.matrix = bodyCoordinatesMat;
-      leg2.matrix.setTranslate(-0.15, -0.2, 0.05); 
-      leg2.matrix.rotate(-g_legsAngle,1,0,0);
-      leg2.matrix.scale(0.05, -0.45, 0.05);
-      leg2.render();
-
-      var leg3 = new Cube(); // Back left
-      leg3.color = [0.992, 0.961, 0.886, 1.0];
-      leg3.matrix = bodyCoordinatesMat;
-      leg3.matrix.setTranslate(0.05, -0.2, 0.5); 
-      leg3.matrix.rotate(-g_legsAngle,1,0,0);
-      leg3.matrix.scale(0.05, -0.45, 0.05);
-      leg3.render();
-
-      var leg4 = new Cube(); // Back right
-      leg4.color = [0.992, 0.961, 0.886, 1.0];
-      leg4.matrix = bodyCoordinatesMat;
-      leg4.matrix.setTranslate(-0.15, -0.2, 0.5); 
-      leg4.matrix.rotate(g_legsAngle,1,0,0);
-      leg4.matrix.scale(0.05, -0.45, 0.05);
-      leg4.render();
-*/
     
   // Draw the body cube
   var body = new Cube();
